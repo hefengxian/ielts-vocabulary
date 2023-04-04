d6a806d52ec31e3905c6a0ea8ce368a7e45701b6
Frank <jssmith883@gmail.com>
2023-04-04 13:15:56 +0800

    add 10 vacabulary

--- vocabulary.js
+++ vocabulary.js
@@ -3,19 +3,41 @@
  */
 
 const vocabulary = {
-    nature_geograpy: {
+    '自然地理': {
         label: '自然地理',
         words: [
-            {id: 1, word: 'atmosphere', pos: 'n.', meaning: '大气层；气氛', example: '', extra: 'working atmosphere 工作氛围'},
+            {id: 1, word: 'atmosphere', pos: 'n.', meaning: '大气层；气氛', example: '', extra: 'working/relaxing/learning atmosphere 工作/放松/学习氛围'},
             {id: 2, word: 'hydrosphere', pos: 'n.', meaning: '大气中的水汽；水圈', example: '', extra: ''},
             {id: 3, word: 'lithosphere', pos: 'n.', meaning: '岩石圈', example: '', extra: ''},
             {id: 4, word: 'oxygen', pos: 'n.', meaning: '氧气', example: '', extra: 'gases 气体'},
             {id: 5, word: 'oxide', pos: 'n.', meaning: '氧化物', example: '', extra: ''},
             {id: 6, word: 'carbon dioxide', pos: 'n.', meaning: '二氧化碳', example: '', extra: ''},
-            {id: 7, word: 'hydrogen', pos: 'n.', meaning: '氢气', example: '', extra: 'hydro水，gen生成'},
+            {id: 7, word: 'hydrogen', pos: 'n.', meaning: '氢气', example: '', extra: 'hydro水，gen生成；generation gap 代沟'},
             {id: 8, word: 'core', pos: 'n.', meaning: '中心，核心，地核', example: '', extra: ''},
             {id: 9, word: 'crust', pos: 'n.', meaning: '地壳；面包皮', example: '', extra: ''},
             {id: 10, word: 'mantle', pos: 'n.', meaning: '地幔；斗篷披风； v. 覆盖', example: '', extra: ''},
+            {id: 11, word: 'longitude', pos: 'n.', meaning: '经度', example: 'a line of longitude 经线', extra: ''},
+            {id: 12, word: 'latitude', pos: 'n.', meaning: '纬度', example: 'hight latitude', extra: ''},
+            {id: 13, word: 'altitude', pos: 'n.', meaning: '海拔', example: '', extra: ''},
+            {id: 14, word: 'horizon', pos: 'n.', meaning: '地平线', example: 'broaden sb\'s horizon 扩展某人的视野', extra: ''},
+            {id: 15, word: 'disaster', pos: 'n.', meaning: '灾难', example: '', extra: 'adj. disastrous'},
+            {id: 16, word: 'mishap', pos: 'adj.', meaning: '小灾难的', example: '', extra: ''},
+            {id: 17, word: 'catastrophic', pos: 'adj.', meaning: '灾难性的', example: '', extra: ''},
+            {id: 18, word: 'calamity', pos: 'n.', meaning: '灾难不幸', example: '', extra: 'cause a calamity'},
+            {id: 19, word: 'endanger', pos: 'v.', meaning: '灾难不幸', example: '', extra: 'enforce 加强； enlarge 变大'},
+            {id: 20, word: 'jeopardise', pos: 'v.', meaning: '危害、危及', example: '', extra: 'jeopardise sb\'s life'},
         ],
-    }
+    },
+    '植物研究': {
+        label: '植物研究',
+        words: [],
+    },
+    '动物保护': {
+        label: '动物保护',
+        words: [],
+    },
+    '建筑场所': {
+        label: '建筑场所',
+        words: [],
+    },
 }
