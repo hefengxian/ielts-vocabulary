ce49f999c20bfb0716ce127513b4850c634af5d4
Frank <jssmith883@gmail.com>
2023-04-10 00:31:39 +0800

    Add audio play

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const { createApp, reactive, computed, ref } = Vue
+const { createApp, reactive, computed, ref, onMounted, onUpdated } = Vue
 
 const opts = {
     setup() {
         const keyword = ref('')
         const category = ref('')
+        const loaded = ref(false)
         const wordList = computed(() => {
             let result = structuredClone(vocabulary)    // deep clone
             const keywordValue = keyword.value.trim().toLowerCase()
@@ -31,11 +32,55 @@ const opts = {
             }
             return result
         })
+
+        onMounted(() => {
+            loaded.value = true
+
+            // 只能同时播放一个音频
+            const audioTags = document.getElementsByTagName('audio')
+            for (let audio of audioTags) {
+                audio.onplay = () => {
+                    for (let _audio of audioTags) {
+                        _audio.blur()
+                        if (audio != _audio) {
+                            _audio.pause()
+                        }
+                    }
+                }
+            }
+
+        })
+
+        onUpdated(() => {
+            // 音频再切换 SRC 之后需要调用一下 load() 不然看不到效果
+            for (let el of document.getElementsByTagName('audio')) {
+                el.load()
+            }
+        })
+
+        document.addEventListener('keyup', ev => {
+            // 激活的那个音频可以通过方向键进行快进/退
+            if (['ArrowLeft', 'ArrowRight'].includes(ev.key)) {
+                ev.preventDefault()
+                const audioTags = document.getElementsByTagName('audio')
+                for (let audioTag of audioTags) {
+                    if (!audioTag.paused) {
+                        audioTag.blur()
+                        const step = (ev.key === 'ArrowLeft' ? -3 : 3)
+                        audioTag.currentTime = audioTag.currentTime + step 
+                        // console.log(step, audioTag.currentTime)
+                    }
+
+                }
+            }
+        })
+ 
         return {
             keyword,
             wordList,
             vocabulary,
             category,
+            loaded,
         }
     }
 }
--- parse_vocabulary.py
+++ parse_vocabulary.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from collections import defaultdict
+from urllib import request
 
 CUR_DIR = Path(__file__).absolute().parent
 
@@ -12,16 +13,19 @@ def parse():
         3: 'example',
         4: 'extra',
     }
-    result = defaultdict(lambda: {'label': '', 'words': []})
+    result = defaultdict(lambda: {'label': '', 'audio': '', 'words': []})
     vocabulary_path = CUR_DIR / 'vocabulary.txt'
     contents = vocabulary_path.read_text()
     categories = contents.split('===\n')
     cur_id = 0
+    category_index = 0
     for category in categories:
+        category_index += 1
         category_parts = category.split('+++\n')
         label = category_parts[0].strip()
         category_body = result[label]
         category_body['label'] = label
+        category_body['audio'] = f"{str(category_index).zfill(2)}_{label}.mp3"
         word_groups = category_parts[1].split('---\n')
         for word_group in word_groups:
             words = word_group.strip().split('\n')
@@ -32,18 +36,32 @@ def parse():
                 word_dict = {'id': cur_id}
                 for part_index in part_mapping:
                     dict_key = part_mapping[part_index]
-                    word_dict[dict_key] = word_parts[part_index] if part_index < len(word_parts) else '-'
+                    word_dict[dict_key] = word_parts[part_index] if part_index < len(
+                        word_parts) else '-'
                 group.append(word_dict)
             category_body['words'].append(group)
-    return dict(result)
-
-
-if __name__ == '__main__':
+    
     js_code = f"""/**
- * pos = part of speech
- */
+         * pos = part of speech
+         */
 
-const vocabulary = {parse().__str__()}
+        const vocabulary = {dict(result).__str__()}
 """
     vocabulary_js_file = CUR_DIR / 'vocabulary.js'
     vocabulary_js_file.write_text(js_code)
+
+
+def download_audio():
+    names = ['01_自然地理', '02_植物研究', '03_动物保护', '04_太空探索', '05_学校教育', '06_科技发明', '07_文化历史', '08_语言演化', '09_娱乐运动', '10_物品材料', '11_时尚潮流',
+             '12_饮食健康', '13_建筑场所', '14_交通旅行', '15_国家政府', '16_社会经济', '17_法律法规', '18_沙场争锋', '19_社会角色', '20_行为动作', '21_身心健康', '22_时间日期']
+
+    for index in range(22):
+        url = f"https://down.guixue.com/mp3/ielts_lexicon/{index + 1}.mp3"
+        file_name, headers = request.urlretrieve(url)
+        Path(file_name).rename(CUR_DIR / f'audio/{names[index]}.mp3')
+
+
+if __name__ == '__main__':
+    # download_audio()
+    parse()
+    pass
